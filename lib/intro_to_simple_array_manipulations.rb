505dd7b823976eabd31bccd34725d2f764164c75
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-24 03:46:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,11 +2,13 @@ def using_push(array, string)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   colors_in_the_rainbow.push("violet")
 end
+p using_push(nil,nil)
 
 def using_unshift(array, string)
    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
    bouroughs_in_nyc.unshift("Staten Island")
 end
+p using_unshift(nil,nil)
 
 def using_pop(array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
@@ -19,13 +21,4 @@ def pop_with_args(array)
   dog_breeds.pop(2)
 end
 
-def using_shift(array)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  lagos = my_favorite_cities.shift 
-  p lagos
-end
 
-def shift_with_args(array)
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  ice_cream_brands.shift(2)
-end
