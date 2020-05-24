030005b4b7725c8f3f7d4754c737181b8496ed54
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-24 03:19:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,6 @@ p using_unshift(nil,nil)
 def using_pop(array)
   @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   antarctica = @continents.pop
-  p antarctica
+
 end
 p using_pop(nil)
