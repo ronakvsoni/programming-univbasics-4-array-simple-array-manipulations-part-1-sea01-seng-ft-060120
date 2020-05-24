4d3fdcb883df2b8d7f331ca48aabb91e1ed46815
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-24 03:45:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,23 +9,23 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  antarctica = @continents.pop
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  antarctica = continents.pop
   p antarctica
 end
 
 def pop_with_args(array)
-  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  @dog_breeds.pop(2)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds.pop(2)
 end
 
 def using_shift(array)
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  lagos = @my_favorite_cities.shift 
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  lagos = my_favorite_cities.shift 
   p lagos
 end
 
 def shift_with_args(array)
-  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @ice_cream_brands.shift(2)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  ice_cream_brands.shift(2)
 end
